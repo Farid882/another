f0ea2df3468653b395ec30eef4a96be2be1926f6
Farid Sabirjanow <subfarid783@gmail.com>
2024-04-21 15:57:27 +0300

    add  three strings

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Say hello to your machine");
+        System.out.println("Thinking in the air!");
+        System.out.println("More one more");
     }
 }
